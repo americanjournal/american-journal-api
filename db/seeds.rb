7f5710f7849b2d0b8e5c55530b90b1d28b065be4
Aaron Strick <aaron@noredink.com>
2016-11-19 15:21:27 -0800

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+["Security", "Patriotism", "Human Equality"].each do |value_name|
+  value = Value.create(name: value_name)
+
+  ["Aaron", "Jared", "Emily", "Jing", "Rohan"].each do |person_name|
+    u = User.create(
+      username: person_name,
+      age: 20 * rand(1.3).round,
+      candidate: %w{Trump Hillary}[rand(1)]
+    )
+
+    4.times do |idx| 
+      Story.create(value_id: value.id, story: Faker::Hipster.sentence(20), user_id: u.id)
+    end
+  end
+end
+
+
