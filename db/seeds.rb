f8129df06ad8498139728dd06409ee07b17fb0ee
Aaron Strick <aaron@noredink.com>
2016-11-20 15:41:42 -0800

    put in real data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,35 +6,36 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create(age: 30, candidate: "Hillary", username: "aaronstrick")
+# User.create(age: 30, candidate: "Hillary", username: "aaronstrick")
 
-["Security", "Patriotism", "Human Equality"].each do |value_name|
-  value = Value.create(name: value_name, prompt: Faker::Hipster.sentence(8))
+# ["Security", "Patriotism", "Human Equality"].each do |value_name|
+#   value = Value.create(name: value_name, prompt: Faker::Hipster.sentence(8))
 
-  ["Aaron", "Jared", "Emily", "Jing", "Rohan"].each do |person_name|
-    u = User.create(
-      username: person_name,
-      age: 20 * rand(1.3).round,
-      candidate: %w{Trump Hillary}[rand(1)]
-    )
+#   ["Aaron", "Jared", "Emily", "Jing", "Rohan"].each do |person_name|
+#     u = User.create(
+#       username: person_name,
+#       age: 20 * rand(1.3).round,
+#       candidate: %w{Trump Hillary}[rand(1)]
+#     )
 
-    4.times do |idx| 
-      Story.create(value_id: value.id, story: Faker::Hipster.sentence(20), user_id: u.id)
-    end
-  end
+#     4.times do |idx| 
+#       Story.create(value_id: value.id, story: Faker::Hipster.sentence(20), user_id: u.id)
+#     end
+#   end
 
-  num_users = User.count
-  num_stories = Story.count
+#   num_users = User.count
+#   num_stories = Story.count
 
-  100.times do
-    ["love", "inspired"].each do |feeling|
-      rand_user = rand(num_users) + 1
-      rand_story = rand(num_stories) + 1
+#   100.times do
+#     ["love", "inspired"].each do |feeling|
+#       rand_user = rand(num_users) + 1
+#       rand_story = rand(num_stories) + 1
 
-      Reaction.create(feeling: feeling, user_id: rand_user, story_id: rand_story)
-    end
-  end
+#       Reaction.create(feeling: feeling, user_id: rand_user, story_id: rand_story)
+#     end
+#   end
 
-end
+# end
 
 
+Rake::Task['import:data'].invoke
---
+++ lib/tasks/import.rake
@@ -0,0 +1,25 @@
+require "CSV"
+
+namespace :import do
+  desc "Import the data from the form"
+  task :data => :environment do
+    file_name = Rails.root.join('db/data.csv')
+
+    user = User.create(age: 30, candidate: "NoOne", username: "the_global_user")
+
+    CSV.read(file_name).each do |row|
+      value_name = row.shift
+      prompt = row.shift
+
+      puts "value name: #{value_name}"
+      puts "prompt: #{prompt}"
+
+      value = Value.create(name: value_name, prompt: prompt)
+      row.each do |story|
+        if story
+          Story.create(value_id: value.id, story: story, user_id: user.id)
+        end
+      end
+    end
+  end
+end
